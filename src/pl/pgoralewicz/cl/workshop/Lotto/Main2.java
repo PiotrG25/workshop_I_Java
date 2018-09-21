a4ed9e228767981cfe852801fcc277e1ae415d10
Piotr <piotrgoralewicz25@gmail.com>
2018-09-21 14:46:34 +0200

    wykonanie Lotto

--- src/pl/pgoralewicz/cl/workshop/Lotto/Main2.java
+++ src/pl/pgoralewicz/cl/workshop/Lotto/Main2.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
         int[] nums = new int [6];
-        Scanner scanner = new Scanner(System.in);
 
         //wpisywanie liczb uzytkownika
         System.out.println("Wpisz 6 liczb całowitych z zakresu 1-49");
@@ -26,23 +25,63 @@ public class Main2 {
             }
         }
 
-        
+        //Wypisanie liczb uzytkownika na ekran
+        System.out.println("Twoje liczby: " + Arrays.toString(nums));
+
+
+        int[] lnums = new int [6];
+
+        //pierwsze losowanie
+        for(int i = 0; i < lnums.length; i++){
+            lnums[i] = (int)(Math.random() * 49) + 1;
+        }
+        Arrays.sort(lnums);
+
+        //walidacja losowania
+        for(int i = 1; i < lnums.length; i++){
+            while(lnums[i - 1] == lnums[i]){
+                lnums[i] = (int)(Math.random() * 49) + 1;
+                Arrays.sort(lnums);
+            }
+        }
+
+        //Wypisywanie liczb wylosowanych na ekran
+        System.out.println("Wylosowane liczby: " + Arrays.toString(lnums));
+
+        //Zliczanie odgadnietych liczb
+        int ile = 0;
+        for(int i : nums){
+            for(int j : lnums){
+                if(i == j){
+                    ile++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Zgadłeś " + ile + " liczb!");
+        if(ile < 3){
+            System.out.println("Trafiłeś mniej niż 3 liczby, brak nagrody");
+        }
+
     }
+
+    static Scanner scan = new Scanner(System.in);
+
     static int scanInt(){
         //walidated int input
         int n;
-        Scanner scan = new Scanner(System.in);
         while(true){
-            if(!scan.hasNextInt()){
-                scan.next();
-                System.out.println("Podaj liczbę");
-            }else{
+            if(scan.hasNextInt()){
                 n = scan.nextInt();
                 while(n > 49 || n < 1){
                     System.out.println("Liczba musi byc z zakresu 1 49");
                     n = scanInt();
                 }
                 break;
+            }else{
+                scan.next();
+                System.out.println("Podaj liczbę");
             }
         }
         return n;
