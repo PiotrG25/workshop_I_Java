93112439807992fc44797777c17c111ccfa4f9f8
Piotr <piotrgoralewicz25@gmail.com>
2018-09-23 16:18:00 +0200

    wykonanie wyszukiwanie slow Main5

--- src/pl/pgoralewicz/cl/workshop/find_words/Main5.java
+++ src/pl/pgoralewicz/cl/workshop/find_words/Main5.java
@@ -1,7 +1,5 @@
 package pl.pgoralewicz.cl.workshop.find_words;
 
-// jsoup HTML parser library @ https://jsoup.org/
-//compile 'org.jsoup:jsoup:1.11.3'
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -9,7 +7,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main5 {
     public static void main(String[] args) {
@@ -17,9 +21,54 @@ public class Main5 {
         try{
             Document document = connect.get();
             Elements links = document.select("span.title");
-            for(Element elem : links){
-                System.out.println(elem.text());
+
+            String f1Name = "popular_words.txt";
+            File f1 = new File(f1Name);
+            if(!f1.exists()){
+                Files.createFile(Paths.get(f1Name));
             }
+
+            String f2Name = "filtered_popular_words.txt";
+            File f2 = new File(f2Name);
+            if(!f2.exists()){
+                Files.createFile(Paths.get(f2Name));
+            }
+
+            PrintWriter pw1 = new PrintWriter(f1Name);
+            PrintWriter pw2 = new PrintWriter(f2Name);
+
+            String[] excludedWords = {"oraz", "ponieważ", "gdyż", "albo", "ani", "bądź", "aczkolwiek"};
+
+            for(Element elem : links){
+                StringTokenizer st = new StringTokenizer(elem.text()," \"\t\n,.:;?!'");
+
+                while(st.hasMoreTokens()){
+                    String word = st.nextToken();
+
+                    if(word.length() >= 3){
+                        pw1.println(word);
+                    }
+                }
+            }
+            pw1.close();
+
+            Scanner scan = new Scanner(f1);
+            while(scan.hasNext()){
+                String word = scan.next();
+                boolean write = true;
+
+                for(int i = 0; i < excludedWords.length; i++){
+                    if(word.toUpperCase().equals(excludedWords[i].toUpperCase())){
+                        write = false;
+                        break;
+                    }
+                }
+                if(write == true){
+                    pw2.println(word);
+                }
+            }
+            pw2.close();
+
         }catch(IOException e){
             e.printStackTrace();
         }
